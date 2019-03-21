3bf64031bbc99e92b78c1cdae9f6667937165eb5
jerome___ <contacts@jerome-lanteri.com>
2019-03-21 21:42:35 +0700

    Object.py syntax attributes with _ in front.

--- app/components/object.py
+++ app/components/object.py
@@ -5,16 +5,16 @@ The Object class contain each object who can be find inside any room randomly.
 
 class Object:
 
-    logoFile = ""
-    name = ""
+    _logoFile = ""
+    _name = ""
     def __init__(self):
         pass
 
     def setName(self, name):
-        self.name = name
+        self._name = name
 
     def getName(self):
         return self.name
 
     def setLogoFile(self, file):
-        self.logoFile = file
+        self._logoFile = file
